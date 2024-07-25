073fe65199f4952c743cf19d491e18913f02b892
flp-gregorio <felipealvesgregorio01@gmail.com>
2024-07-25 19:52:06 -0300

    create schema for user

---
+++ prisma/migrations/20240725220130_create_users_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,50 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 const app = fastify();
 app.get("/", async (request, reply) => {
-    return { hello: "world" };
+  return { hello: "world" };
 });
 
-app.listen({port:3333}).then(() => {
-    app.log.info(`server listening}`);
+app.listen({ port: 3333 }).then(() => {
+  app.log.info(`server listening}`);
+});
+
+app.post("/users", async (request, reply) => {
+  const { email, username, password } = request.body;
+  const user = await prisma.user.create({
+    data: {
+      email,
+      username,
+      password,
+    },
+  });
+  reply.send(user);
+});
+
+app.get("/users", async (request, reply) => {
+  const users = await prisma.user.findMany();
+  reply.send(users);
+});
+
+app.delete("/users/:id", async (request, reply) => {
+  const { id } = request.params;
+  const user = await prisma.user.delete({
+    where: { id: String(id) },
+  });
+  reply.send(user);
+});
+
+app.put("/users/:id", async (request, reply) => {
+  const { id } = request.params;
+  const { email, username, password } = request.body;
+  const user = await prisma.user.update({
+    where: { id: String(id) },
+    data: {
+      email,
+      username,
+      password,
+    },
+  });
+  reply.send(user);
 });
